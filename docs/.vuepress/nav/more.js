440948917927d7a76aa568fd69ca00da2abd42d2
Auorui <ZJR@666.com>
2023-06-22 22:54:54 +0800

    更新内容 + Action 自动部署

--- docs/.vuepress/nav/more.js
+++ docs/.vuepress/nav/more.js
@@ -4,6 +4,7 @@ module.exports = {
   items: [
     // { text: "友情链接", link: "/more/friend/8e1a3d/" },
     { text: "免费信息", link: "/more/free/" },
+    { text: "GitHub精选", link: "/more/github/" },
     { text: "时间线", link: "/archives/" },
     { text: "关于", link: "/about/" },
   ],
